d56799b6bafced882649fe8c0a5d81ffa312aa58
Naveen Kulkarni <34886268+naveen-kulkarni0@users.noreply.github.com>
2020-01-29 11:46:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const { Storage } = require("@google-cloud/storage");
+
+console.log(Storage);
+
 exports.helloWorld = (req, res) => {
   let message = req.query.message || req.body.message || 'Heyy World!';
   res.status(200).send(message);
